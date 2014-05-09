17f392cc26a5493b8658ceeba97433272b97cea7
giovannibotta <amojobim@gmail.com>
2014-05-09 13:15:26 -0400

    Added almost full implementation of java.util.Map interface to the open addressing hash map

--- datastructures/src/main/java/net/giovannibotta/hashtable/OpenAddressingHashTable.java
+++ datastructures/src/main/java/net/giovannibotta/hashtable/OpenAddressingHashTable.java
@@ -1,5 +1,6 @@
 package net.giovannibotta.hashtable;
 
+import java.util.*;
 import java.util.function.BiPredicate;
 import java.util.function.ToIntBiFunction;
 import java.util.function.ToIntFunction;
@@ -8,11 +9,11 @@ import java.util.function.ToIntFunction;
  * @author giovanni
  * @since 5/7/14
  */
-public class OpenAddressingHashTable<K, V> {
+public class OpenAddressingHashTable<K, V> extends AbstractMap<K, V> {
     private final ToIntFunction<K> hash1;
     private final ToIntBiFunction<K, Integer> hash2;
     private final BiPredicate<K, K> equals;
-    private Entry<K, V> table[];
+    private SimpleImmutableEntry<K, V> table[];
     private int size = 0;
 
     public static <K, V> OpenAddressingHashTable<K, V> createLinear(ToIntFunction<K> hash, BiPredicate<K, K> equals) {
@@ -57,7 +58,7 @@ public class OpenAddressingHashTable<K, V> {
             // keep track of the first entry marked as deleted
             if (table[pos] == deleted && firstDeleted == -1) firstDeleted = pos;
             // if I found the key, I'm done
-            if (eq(table[pos].k, k)) {
+            if (eq(k, table[pos].getKey())) {
                 if (firstDeleted >= 0) {
                     // move the found entry to the first deleted entry if any
                     table[firstDeleted] = table[pos];
@@ -68,65 +69,127 @@ public class OpenAddressingHashTable<K, V> {
             }
             // compute next index
             pos = (h + hash2(k, ++i)) % table.length;
+            // mod could be negative
+            while (pos < 0) pos += table.length;
         }
         // if I got here, I went through the whole table without any match
         // note that if the table is not full, this will return -(x+1) where x is the position where the item should be put
-        return firstDeleted >= 0 ? -(firstDeleted + 1) : -(pos + 1);
+        // or -(table.length + 1) if the position of the element in the table could not be determined (could happen with double hashing)
+        if (firstDeleted >= 0) return -(firstDeleted + 1);
+        if (table[pos] == null) return -(pos + 1);
+        return -(table.length + 1);
+//        return firstDeleted >= 0 ? -(firstDeleted + 1) : -(pos + 1);
     }
 
-    public boolean containsKey(K k) {
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean containsKey(Object k) {
+        if (k == null) throw new NullPointerException("Null key");
+        K key = (K) k;
+        return safeContainsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        for (SimpleImmutableEntry<K, V> e : table)
+            if (e.getValue() == value || (value != null && value.equals(e.getValue()))) return true;
+        return false;
+    }
+
+    public boolean safeContainsKey(K k) {
         return find(k) >= 0;
     }
 
-    public V get(K k) {
+    @Override
+    public V get(Object k) {
+        K key = fromObject(k);
+        return safeGet(key);
+    }
+
+    @SuppressWarnings("unchecked")
+    private K fromObject(Object k) {
+        if (k == null) throw new NullPointerException("Null key");
+        return (K) k;
+    }
+
+    @Override
+    public V remove(Object key) {
+        K k = fromObject(key);
         int pos = find(k);
-        if (pos >= 0) return table[pos].v;
+        if (pos < 0) return null;
+        V old = table[pos].getValue();
+        table[pos] = deleted;
+        size--;
+        return old;
+    }
+
+    public V safeGet(K k) {
+        int pos = find(k);
+        if (pos >= 0) return table[pos].getValue();
         return null;
     }
 
-    private boolean blindPut(K k, V v) {
-        int pos = find(k);
+    @Override
+    public void clear() {
+        this.size = 0;
+        table = null;
+        checkSize();
+    }
+
+    private V blindPut(SimpleImmutableEntry<K, V> newEntry) {
+        int pos = find(newEntry.getKey());
         if (pos >= 0) {
-            if ((v != null && v.equals(table[pos].v)) || table[pos].v == null) return false;
-            table[pos].v = v;
-            return true;
+            V old = table[pos].getValue();
+            table[pos] = newEntry;
+            return old;
         } else {
-            table[-pos - 1] = new Entry<>(k, v);
+            int j = -pos - 1;
+            if (j >= table.length) throw new RuntimeException("Unable to position key " + newEntry.getKey());
+            table[-pos - 1] = newEntry;
             size++;
-            return true;
+            return null;
         }
     }
 
-    public boolean put(K k, V v) {
+    @Override
+    public V put(K k, V v) {
         if (k == null) throw new IllegalArgumentException("Key can not be null");
         checkSize();
-        return blindPut(k, v);
+        return blindPut(new SimpleImmutableEntry<>(k, v));
+    }
+
+    @Override
+    public Set<Map.Entry<K, V>> entrySet() {
+        throw new UnsupportedOperationException("Method entrySet not supported at this moment");
     }
 
     @SuppressWarnings("unchecked")
     private void checkSize() {
-        if (table == null) table = new Entry[1];
+        if (table == null) table = new SimpleImmutableEntry[1];
         if (size == table.length || size < table.length / 4) {
             int newSize = 2 * table.length;
             if (size < table.length / 4) {
                 newSize = Math.max(1, table.length / 2);
             }
 
-            Entry<K, V>[] oldTable = table;
-            table = new Entry[newSize];
+            SimpleImmutableEntry<K, V>[] oldTable = table;
+            table = new SimpleImmutableEntry[newSize];
 
             // reset the size because blindPut will increment it
             size = 0;
-            for (Entry<K, V> e : oldTable)
+            for (SimpleImmutableEntry<K, V> e : oldTable)
                 if (e != null && e != deleted)
-                    blindPut(e.k, e.v);
+                    blindPut(e);
         }
     }
 
+
+    @Override
     public int size() {
         return size;
     }
 
+    @Override
     public boolean isEmpty() {
         return size == 0;
     }
@@ -134,21 +197,72 @@ public class OpenAddressingHashTable<K, V> {
     @Override
     public String toString() {
         StringBuilder b = new StringBuilder("{ ");
-        for (Entry<K, V> e : table) if (e != null && e != deleted) b.append(e.k).append(" : ").append(e.v).append(", ");
+        for (SimpleImmutableEntry<K, V> e : table)
+            if (e != null && e != deleted) b.append(e.getKey()).append(" -> ").append(e.getValue()).append(", ");
         if (size > 0) b.delete(b.length() - 2, b.length());
         return b.append(" }").toString();
     }
 
     // the key can never be null so null here is fine
-    private final Entry<K, V> deleted = new Entry<>(null, null);
+    private final SimpleImmutableEntry<K, V> deleted = new SimpleImmutableEntry<>(null, null);
 
-    public static class Entry<KK, VV> {
-        private final KK k;
-        private VV v;
+    private class EntrySet extends AbstractSet<Entry<K, V>> {
+        @Override
+        public int size() {
+            return OpenAddressingHashTable.this.size();
+        }
 
-        public Entry(KK key, VV value) {
-            this.k = key;
-            this.v = value;
+        @Override
+        public boolean isEmpty() {
+            return OpenAddressingHashTable.this.isEmpty();
+        }
+
+        @SuppressWarnings("unchecked")
+        private Entry<K, V> fromObject(Object o) {
+            if (o == null) throw new NullPointerException("Null entry");
+            Entry<K, V> e = (Entry<K, V>) o;
+            return e;
+        }
+
+        @Override
+        public boolean contains(Object o) {
+            Entry<K, V> e = fromObject(o);
+
+            if (!OpenAddressingHashTable.this.containsKey(e.getKey())) return false;
+
+            V val = OpenAddressingHashTable.this.get(e.getKey());
+            return val == e.getValue() || (val != null && val.equals(e.getValue()));
+        }
+
+        @Override
+        public Iterator<Entry<K, V>> iterator() {
+            return null;  //To change body of implemented methods use File | Settings | File Templates.
+        }
+
+        @Override
+        public boolean add(Entry<K, V> kvEntry) {
+            throw new UnsupportedOperationException("Can not add to this view using the add method");
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            Entry<K, V> e = fromObject(o);
+            return OpenAddressingHashTable.this.remove(e.getKey()) != null;
+        }
+
+        @Override
+        public boolean addAll(Collection<? extends Entry<K, V>> c) {
+            throw new UnsupportedOperationException("Can not add to this view using the addAll method");
+        }
+
+        @Override
+        public void clear() {
+            OpenAddressingHashTable.this.clear();
+        }
+
+        @Override
+        public String toString() {
+            return OpenAddressingHashTable.this.toString();
         }
     }
 }
--- datastructures/src/test/java/net/giovannibotta/hashtable/OpenAddressingHashTableTest.java
+++ datastructures/src/test/java/net/giovannibotta/hashtable/OpenAddressingHashTableTest.java
@@ -16,6 +16,7 @@ public class OpenAddressingHashTableTest {
 
         basicTest(OpenAddressingHashTable.createQuadratic(String::hashCode, String::equals));
 
+        // FIXME: this is broken right now
         basicTest(OpenAddressingHashTable.createDoubleHashing(String::hashCode, String::hashCode, String::equals));
     }
 
@@ -24,11 +25,11 @@ public class OpenAddressingHashTableTest {
         assertEquals("Table size initially 0", 0, table.size());
         assertTrue("Table does not contain some key", !table.containsKey("x"));
 
-        assertTrue("Put operation modifies table", table.put("x", "y"));
+        assertEquals("Put operation modifies table", null, table.put("x", "y"));
         assertEquals("Can find a given key and retrieve the correct value", "y", table.get("x"));
-        assertTrue("Put operation modifies table", table.put("y", "y"));
+        assertEquals("Put operation modifies table", null, table.put("y", "y"));
         assertEquals("Can find a given key and retrieve the correct value", "y", table.get("x"));
-        assertTrue("Put operation modifies table", table.put("z", "y"));
+        assertEquals("Put operation modifies table", null, table.put("z", "y"));
         assertEquals("Can find a given key and retrieve the correct value", "y", table.get("y"));
         assertEquals("Can find a given key and retrieve the correct value", "y", table.get("z"));
         assertEquals("Table size is correct", 3, table.size());
@@ -36,12 +37,26 @@ public class OpenAddressingHashTableTest {
         assertTrue("Can find a given key", table.containsKey("x"));
         assertEquals("Can find a given key and retrieve the correct value", "y", table.get("x"));
 
-        assertTrue("Put operation modifies table if value is different", table.put("x", "z"));
-        assertTrue("Put operation does not modify table if value is the same", !table.put("x", "z"));
+        assertEquals("Put operation modifies table if value is different", "y", table.put("x", "z"));
+        assertEquals("Put operation does not modify table if value is the same", "z", table.put("x", "z"));
         assertEquals("Updates to the same key does not change the size", 3, table.size());
 
         assertTrue("Can find a given key", table.containsKey("x"));
         assertEquals("Can find a given key and retrieve the correct value", "z", table.get("x"));
+
+        assertEquals("Key correctly removed", "y", table.remove("z"));
+        assertEquals("Updated size after deletion", 2, table.size());
+
+        table.put("a", "1");
+        table.put("b", "2");
+        table.put("c", "3");
+
+        assertEquals("Correct size", 5, table.size());
+        assertEquals("Can find a given key and retrieve the correct value", "1", table.get("a"));
+        assertEquals("Can find a given key and retrieve the correct value", "2", table.get("b"));
+
+        assertEquals("Key correctly removed", "1", table.remove("a"));
+        assertEquals("Updated size after deletion", 4, table.size());
 
         System.out.println(table);
     }
